17a0d9344d36f1caaf556c319f5612394f7cdcab
EscortSC <104186731+EscortSC@users.noreply.github.com>
2022-11-13 13:16:09 +0800

    Delete Escort directory

--- Escort/src/spirit/core/design/DesignFlaw.java
+++
@@ -1,5 +0,0 @@
-/** <a href="http://www.cpupk.com/decompiler">Eclipse Class Decompiler</a> plugin, Copyright (c) 2017 Chen Chao. **/
-package spirit.core.design;
-
-public abstract class DesignFlaw {
-}
